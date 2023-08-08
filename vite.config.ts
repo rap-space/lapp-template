d0a5f2ce2bee9f72f2b6a62efdff5a91fdac65ca
lucien.ly <lucien.ly@alibaba-inc.com>
2023-08-08 12:01:31 +0800

    feat: 区分环境，方便调试

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { getJsonOrDie } from './script/utils.mjs'
 
 /** 根据命令行入参，定义模式常量 */
 const MODE = process.argv.indexOf('--watch') !== -1 ? 'development' : 'production';
+
 /** 构造静态资源依赖路径 */
 const { appKey, version } = getJsonOrDie('package.json');
 const base = `https://1688-lapp.oss-cn-hangzhou.aliyuncs.com/pc-pc_work-pc_work_plugin-${appKey}/${version}/`;
@@ -12,6 +13,10 @@ const base = `https://1688-lapp.oss-cn-hangzhou.aliyuncs.com/pc-pc_work-pc_work_
 // https://vitejs.dev/config/
 export default defineConfig({
   mode: MODE,
+  define: {
+    // 指定 NODE_ENV 方便 react 等库动态依赖不同子包
+    'process.env.NODE_ENV': JSON.stringify(MODE),
+  },
   plugins: [
     react(),
     lappPlugin(),
@@ -19,6 +24,7 @@ export default defineConfig({
   base,
   build: {
     sourcemap: MODE === 'development',
+    minify: MODE === 'production',
     target: 'es2015',
     outDir: 'build',
     rollupOptions: {
