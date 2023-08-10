8c9fc6b346fe1382a5ce83eb0a822cef8afeadbc
lucien.ly <lucien.ly@alibaba-inc.com>
2023-08-10 11:29:15 +0800

    feat: 增加本地代理说明指引

--- lapp-vite-plugin.ts
+++ lapp-vite-plugin.ts
@@ -9,10 +9,18 @@ export default function lappPlugin() {
   const BUILD_DEST = 'build';
   const META_DEST = 'lapp-meta.json';
   const { appKey, version } = getJsonOrDie('package.json');
+  if (typeof appKey !== 'string') {
+    gracefulSuicide('Please make sure appKey is defined in package.json');
+  }
+  if (typeof version !== 'string') {
+    gracefulSuicide('Please make sure version is defined in package.json');
+  }
+  
   let browserOpened = false;
   return {
     name: 'lapp-plugin',
     async closeBundle(error) {
+      let lappMetaStrFormatted = '';
       try {
         // 使用允许注释的宽松 jsonc 语法解析 meta 文件
         const lappMetaBuffer = fs.readFileSync(META_DEST);
@@ -20,17 +28,32 @@ export default function lappPlugin() {
         if (err) {
           gracefulSuicide(`Failed to parse JSON: ${err.message}`, err);
         }
-        const lappMetaStrFormatted = JSON.stringify(result, null, 4);
+        lappMetaStrFormatted = JSON.stringify(result, null, 4);
         // 转换为规整合法的 json 格式写入构建产物
         fs.writeFileSync(path.join(BUILD_DEST, META_DEST), lappMetaStrFormatted);
-        console.log(chalk.blue(`\nlapp-meta:\n${lappMetaStrFormatted}`));
       } catch (error) {
         gracefulSuicide(`File ${META_DEST} doesn't exsist, please make sure you've configured it right !`, error);
       }
       if (process.argv.indexOf('--watch') !== -1 && browserOpened === false) {
-        // 开发态下，初次打包完成，打开调试页面
+        // 开发态下，初次打包完成，提示调试信息，并打开页面
         const devUrl = `https://page.1688.com/html/isv-bridge.html?appKey=${appKey}&version=${version}`;
-        logger.info('Opening dev URL:\n', devUrl);
+        const buildDestFullPath = path.resolve(path.join(__dirname, BUILD_DEST));
+        const proxyRule = `^***1688-lapp.oss-cn-hangzhou.aliyuncs.com/pc-pc_work-pc_work_plugin-${appKey}/*/*** file://${buildDestFullPath}/$3`;
+        console.log(chalk.cyan('\n====================== Environment Info =====================\n'))
+        console.log(chalk.cyan(`
+lapp-meta.json:
+${lappMetaStrFormatted}
+
+Please use whistle as local proxy server:
+${chalk.underline('https://www.npmmirror.com/package/whistle')}
+
+Example for whistle proxy rule:
+${proxyRule}
+
+Local Dev URL:
+${chalk.underline(devUrl)}
+`));
+        console.log(chalk.cyan('\n====================== Environment Info =====================\n'))
         open(devUrl);
         browserOpened = true;
       }
--- script/utils.mjs
+++ script/utils.mjs
@@ -2,24 +2,27 @@ import fs from 'fs';
 import chalk from 'chalk'; // https://www.npmjs.com/package/chalk
 
 /** 输出日志 */
+const chalkWrapper = (chalkFunc, arg) => typeof arg === 'string' ? chalkFunc(arg) : arg;
 export const logger = {
   log: (...args) => {
-    console.log(chalk.blue('[log]'), ...args)
+    console.log(chalk.blue('[log]'), ...args.map(arg => chalkWrapper(chalk.blue, arg)))
   },
   error: (...args) => {
-    console.log(chalk.red('[error]'), ...args)
+    console.log(chalk.red('[error]'), ...args.map(arg => chalkWrapper(chalk.red, arg)))
   },
   info: (...args) => {
-    console.log(chalk.green('[info]'), ...args)
+    console.log(chalk.green('[info]'), ...args.map(arg => chalkWrapper(chalk.green, arg)))
   },
 }
 
 /** 进程优雅自杀 */
 export const gracefulSuicide = (msg, err) => {
-  console.log(chalk.red('\n================= [build error] ================='))
+  console.log(chalk.red('\n================= [build error] =================\n'))
   console.log(chalk.red(msg))
-  console.error(err);
-  console.log(chalk.red('================= [build error] =================\n'))
+  if (err) {
+    console.error(err);
+  }
+  console.log(chalk.red('\n================= [build error] =================\n'))
   process.exit(1);
 }
 
